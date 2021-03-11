c66edeebb54249fb292e2ef3268757bcad481d65
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2021-03-11 22:23:15 +0100

    testando os methodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,14 +1,90 @@
 const  express = require('express')
-
+const { uuid } = require('uuidv4')
 const app = express()
 
 
-app.get('/', (requeste, response) => {
-    return response.json({
-        message: "Hello word"
-    })
+//app.use(express.urlencoded({ extended : true}))
+app.use(express.json())
+
+const allProjects  = [];
+
+
+
+app.get('/projects', (requeste, response) => {
+ 
+ return response.json(allProjects)
 })
 
-app.listen(3333, ()=> {
-    console.log('o servidor esta rodando')
+
+
+app.post('/projects' , (requeste, response) =>{
+
+    const { nameProject, owner } = requeste.body
+
+    const id  = uuid()
+
+    console.log(owner)
+    allProjects.push( {
+        id,
+        nameProject,
+        owner
+        
+    }
+
+
+    )
+
+
+    return response.json(allProjects)
+
+    
 })
+
+app.put('/projects/:id' , (requeste, response) =>{
+
+    
+
+
+    const  { id }  = requeste.params;
+    let index = 0
+
+    const  findProject  = allProjects.find((projectfind, findIndex, arr) => {
+
+        if(projectfind.id == id) {
+            index = findIndex
+            return projectfind
+        }
+ 
+    
+    })
+
+    if(!findProject) return response.status(400).json({error: "project Not_found"})
+
+    const  project = {
+        id,
+        ...requeste.body
+    }
+    allProjects[index] = project
+
+    return response.json(allProjects)
+})
+
+app.delete('/projects/:id' , (requeste, response) =>{
+
+   const { id } = requeste.params 
+
+   const findIndexProject = allProjects.findIndex(project => project.id === id)
+
+   if(findIndexProject < 0) return response.status(400).json({error: "project not found"})
+
+    
+   allProjects.splice(findIndexProject,1)
+
+   return response.status(204).send()
+}) 
+
+
+
+app.listen(3333, ()=> {
+    console.log('Server  is started! 🚀🚀') 
+}) 
